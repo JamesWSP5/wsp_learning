7dad9ec47907849c3174cc9fc530694121ca83f2
511891957@qq.com <511891957@qq.com>
2019-09-10 13:27:36 +0800

    构建项目

---
+++ python/python.md
@@ -0,0 +1,7 @@
+###本项目主要用于python学习
+
+- python基础
+- python爬虫
+- python数据分析
+- python_web
+- python学习资料
---
+++ python/python_basic/day01_hello_world/hello_world.py
@@ -0,0 +1,8 @@
+# 定以一个方法
+def func1():
+    print('hello,world! python')
+
+
+# 在主方法内调用该方法
+if __name__ == '__main__':
+    func1()
