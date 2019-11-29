112079b0cbedf750d9d6128a05b73a0e4520dbc4
511891957@qq.com <511891957@qq.com>
2019-11-29 16:31:20 +0800

    company submit

---
+++ python/python_basic/day01_hello_world/data_type.py
@@ -0,0 +1,8 @@
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+print(L)
+
+for i in range(3):
+    print(i)
+
+l1 = list(range(100))
+print(l1)
--- python/python_data_analysis/spark_pratice/day01/aa.py
+++ python/python_data_analysis/spark_pratice/day01/aa.py
@@ -2,10 +2,23 @@ def test():
     for i in range(3):
         print "aa"
         i = i + 1
-        if i==2:
+        if i == 2:
+            # break
             continue
         print i
 
 
+def test1():
+    import string
+    l = string.ascii_letters
+    s = [l[34], l[11], l[14], l[21], l[4], l[24], l[14], l[20]]
+    s.insert(1, " ")
+    s.insert(6, " ")
+    string = "".join(s)
+    print(string)
+
+
 if __name__ == '__main__':
     test()
+    test1()
+    print ("")
