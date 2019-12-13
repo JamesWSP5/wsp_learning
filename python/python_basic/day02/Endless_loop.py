a0365da7167685903cb86c9b5f31a8bb88e7e2c7
511891957@qq.com <511891957@qq.com>
2019-12-13 17:30:18 +0800

    文件操作

---
+++ python/python_basic/day02/Endless_loop.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+# @Time : 2019/12/13 16:50
+# @Author: wsp
+import time
+
+
+def endless_loop():
+    """
+    死循环
+    :return:None
+    """
+    i = 0
+    while True:
+        i = i + 1
+        if i < 50:
+            print(i)
+        time.sleep(1)
+
+
+def file_opreate():
+    """
+    文件操作  Read Write
+    :return:None
+    """
+    f = open("../source/in.txt", mode="r", encoding="utf-8")
+    content = f.read()
+    print(content)
+    f1 = open("../source/out.txt", mode="a+", encoding="utf-8")
+    f1.write(content)
+    f.close()
+
+
+if __name__ == '__main__':
+    # endless_loop()
+    file_opreate()
