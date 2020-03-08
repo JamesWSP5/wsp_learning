5c4122015d8a8e31406f0d848c542e0cf1a2b52f
511891957@qq.com <511891957@qq.com>
2020-03-08 17:05:07 +0800

    v

---
+++ python/python_basic/day02/dataStruct.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+#@Time : 2020/3/6 17:10
+#@Author: wsp
+
+# 对比java中的数据类型
+# python 中的数据类型
+
+
+import sys
+
+print('命令行参数如下:')
+for i in sys.argv:
+    print(i)
+
+print('\n\nPython 路径为：', sys.path, '\n')
